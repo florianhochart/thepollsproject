e8b323996ec2278c5b74125e3b984d6631868866
Florian Hochart <travail.hochart@gmail.com>
2022-08-27 21:53:15 +0200

    Init SweetAlert2

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
 	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" type="text/css" href="./library/css/style.css">
 	<script type="text/javascript" src="library/lib/jquery-3.6.1.min.js"></script>
+	<script type="text/javascript" src="library/lib/sweetalert2.all.min.js"></script>
 </head>
 
 <header><?php echo $title; ?><hr></header>
--- library/js/script.js
+++ library/js/script.js
@@ -1,3 +1,10 @@
 $(function () {
 	console.log("JQUERY IS ON");
+
+	Swal.fire({
+	  title: 'Error!',
+	  text: 'Do you want to continue',
+	  icon: 'error',
+	  confirmButtonText: 'Cool'
+	})
 });
